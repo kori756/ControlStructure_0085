f1b4475442acc0e7bca4e1d3a84d2967bc5c8707
kori756 <khorirachmatr@gmail.com>
2026-09-22 11:11:56 +0700

    mencari angka terbesar

--- Control source/nomor2.py
+++ Control source/nomor2.py
@@ -2,9 +2,15 @@ nomor_1 = int (input ("masukkan nomor pertama: "))
 nomor_2 = int (input ("masukkan nomor kedua: "))
 nomor_3 = int (input ("masukkan nomor ketiga: "))
 
-nomor = [nomor_1, nomor_2, nomor_3]
-nomor_terbesar = max(nomor)
-print ("nomor terbesar adalah: ", nomor_terbesar)
+if nomor_1 > nomor_2 and nomor_1 > nomor_3:
+    print ("nomor terbesar adalah: ", nomor_1)
+elif nomor_2 > nomor_1 and nomor_2 > nomor_3:
+    print ("nomor terbesar adalah:", nomor_2)
+elif nomor_3 > nomor_1 and nomor_3 > nomor_2:
+    print ("nomor terbesar adalah:", nomor_3)
+else:
+    print ("nomor terbesar sama")
+    
 
 
 
