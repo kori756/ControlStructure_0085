5758eaf888de311d1717cbcbf152950a948d772d
kori756 <khorirachmatr@gmail.com>
2026-09-22 10:47:59 +0700

    Mencari angka terbesar

---
+++ Control source/nomor2.py
@@ -0,0 +1,11 @@
+nomor_1 = int (input ("masukkan nomor pertama: "))
+nomor_2 = int (input ("masukkan nomor kedua: "))
+nomor_3 = int (input ("masukkan nomor ketiga: "))
+
+nomor = [nomor_1, nomor_2, nomor_3]
+nomor_terbesar = max(nomor)
+print ("nomor terbesar adalah: ", nomor_terbesar)
+
+
+
+
