da181c9ae06c32adfb3e167eb15140289f864a91
kori756 <khorirachmatr@gmail.com>
2026-09-22 10:33:06 +0700

    Mmebuat percabangan

---
+++ Control source/nomor1.py
@@ -0,0 +1,13 @@
+Performance = int (input ("Enter performance: "))
+if Performance >= 90:
+    print ('Excellent!')
+elif Performance >= 80:
+    print ('very good performance!')
+elif Performance >= 70:
+    print ('good performance!')
+elif Performance >= 60:
+    print ('average performance!')
+else:
+    print ('poor')
+
+    
